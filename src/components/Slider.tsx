17f2fc78da2ed1a2ae312250936abd4b40071d9a
alenmonti <amonti@fi.uba.ar>
2025-10-05 18:34:43 -0300

    slider fix

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -11,31 +11,31 @@ import foto10 from '/foto10.jpeg';
 
 const Slider = () => {
   return (
-    <div className="h-[400px] w-full">
+    <div className="h-[400px] w-full my-4">
       <Carousel leftControl=" " rightControl=" " slideInterval={1500}>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto3} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto3} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto4} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto4} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto5} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto5} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto6} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto6} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto7} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto7} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto8} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto8} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto9} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto9} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
-        <div className='w-[85%] rounded-lg'>
-          <img src={foto10} alt="..." className=' w-full max-h-[375px] rounded-lg' />
+        <div className='w-[85%] rounded-lg flex justify-center items-center'>
+          <img src={foto10} alt="..." className=' h-full max-h-[400px] rounded-lg' />
         </div>
       </Carousel>
     </div>
